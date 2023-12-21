ad36c522a33be506072830262798d78b984588f1
Jay Dhanwalkar <134925314+dhanwalkarjay@users.noreply.github.com>
2023-12-21 18:48:32 +0530

    Update 2. Remove Character.cpp

--- 2. Remove character.cpp
+++ 2. Remove character.cpp
@@ -3,23 +3,23 @@
 class Solution {
   public:
     string removeChars(string string1, string string2) {
-        // code here
-        // set to store distinct chars in string2
-        unordered_set<char> s2(string2.begin(), string2.end());
+        int n1 = string1.size();
+        int n2 = string2.size();
 
-        int curr=0;
-        for(int i=0;i<string1.size();i++){
-            if(s2.find(string1[i])!=s2.end())
-                continue;
-            string1[curr++]=string1[i]; // overwriting
+        for (int i = 0; i < n2; i++) {
+            for (int j = 0; j < n1; ) {
+                if (string2[i] == string1[j]) 
+                    string1.erase(j, 1); 
+                else
+                    j++;
+            }
         }
-        string1.erase(string1.begin()+curr, string1.end());
         return string1;
     }
 };
 
 /*
-Time complexity: O(|string1|)
+Time complexity: O(|n1*n2|)
 Space complexity: O(|string2|)
 */
 
